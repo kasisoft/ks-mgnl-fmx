22ad89ca6c0dfe0dedcf087a0c52fc52a3e52fca
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-12-09 04:26:44 +0100

    + added option 'trim' to 'CleanDirective'

--- src/main/java/com/kasisoft/mgnl/fmx/internal/CleanDirective.java
+++ src/main/java/com/kasisoft/mgnl/fmx/internal/CleanDirective.java
@@ -16,13 +16,26 @@ public class CleanDirective  implements TemplateDirectiveModel {
 
   private static final Bucket<StringWriter> WRITERS = new Bucket<>( BucketFactories.newStringWriterFactory() );
   
+  private boolean isTrim( Map params ) throws TemplateException {
+    TemplateBooleanModel model = (TemplateBooleanModel) params.get( "trim" );
+    boolean result = false;
+    if( model != null ) {
+      result = model.getAsBoolean();
+    }
+    return result;
+  }
+  
   @Override
   public void execute( Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body ) throws TemplateException, IOException {
     StringWriter writer = null;
     try {
       writer = WRITERS.allocate();
       body.render( writer );
-      env.getOut().write( writer.toString().replaceAll( "([\n]\\s*[\n])", "\n" ) );
+      String content = writer.toString().replaceAll( "([\n]\\s*[\n])", "\n" );
+      if( isTrim( params ) ) {
+        content = content.trim();
+      }
+      env.getOut().write( content );
     } finally {
       if( writer != null ) {
         WRITERS.free( writer );
